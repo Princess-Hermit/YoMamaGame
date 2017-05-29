c5dd8433a83dd8c84c81e220e11b0b745e46ed28
Joshua-J <Joshua@Joshuas-Air.attlocal.net>
2017-05-29 10:48:40 -0700

    Online Leaderboard Added
    Joshua Added an online leaderboard that the player can add his high
    score to.  The leaderboard keeps track of the high scores.  The code is
    written so it is easy to incorporate the scenes into a unity project.

---
+++ Assets/Scripts/Leaderboard Scripts/LB_GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LB_GameController : MonoBehaviour {
+
+    public Text scoreUIText;
+
+	// Use this for initialization
+	void Start () {
+        // set the score text to a random number at the start of the scene
+        scoreUIText.text = Random.Range(0, 10000).ToString();
+	}
+
+    public void GameOver()
+    {
+        // for this example, we just take the contents of the scoreUIText text object
+        // and convert it into a number to use as the final score
+        int finalScore = System.Int32.Parse(scoreUIText.text);
+
+        // when the game has finished, we just need to store the
+        // final score in a PlayerPref so that the leaderboard script
+        // can pick it up later
+        PlayerPrefs.SetInt("finalScore", finalScore);
+
+        // load the leaderboard screen to submit the score
+        SceneManager.LoadScene("AddNewHighScore");
+    }
+
+	public void goToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
---
+++ Assets/Scripts/Leaderboard Scripts/LB_MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LB_MainMenuController : MonoBehaviour {
+
+	public void PlayGame()
+    {
+        SceneManager.LoadScene("Test_Game");
+    }
+
+    public void ViewLeaderboard()
+    {
+        SceneManager.LoadScene("HighScoreTable");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
