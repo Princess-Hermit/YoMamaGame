43b8627f3d15a8c52a661b1a23f13137b04fcd14
ccross <coffeewithcross@gmail.com>
2017-07-04 12:57:18 -0700

    Prep iOS Build
    This needs to be merged with iOS branch and a separate set up for
    Android

--- Assets/Scripts/SnapBrodyScript.cs
+++ Assets/Scripts/SnapBrodyScript.cs
@@ -10,6 +10,6 @@ public class SnapBrodyScript : MonoBehaviour {
     {
         GameObject Brody = GameObject.FindGameObjectWithTag("Brody");
         buttonPosition = gameObject.transform.position;
-        Brody.transform.position = (buttonPosition + new Vector3(20f, 30f));
+        Brody.transform.position = (buttonPosition + new Vector3(20f, 36f));
     }
 }
